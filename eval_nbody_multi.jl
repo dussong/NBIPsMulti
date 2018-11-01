3556077d5afa6737b94692afd72e5f56d64ba011
dussong <dusson@ljll.math.upmc.fr>
2018-11-01 15:42:56 +0000

    few tries.

--- eval_nbody_multi.jl
+++ eval_nbody_multi.jl
@@ -2,11 +2,23 @@ using JuLIP, NeighbourLists
 using JuLIP: AbstractCalculator
 using JuLIP.Potentials: @pot
 
-using NBodyIPs: NBodyFunction, bapolys, eval_site_nbody!, evaluate
+using NBodyIPs: NBodyFunction, bapolys, eval_site_nbody!, evaluate, eval_site_nbody!, evaluate_d!
 
 import JuLIP: site_energies, energy, forces
 
+using NeighbourLists: nbodies,
+                      maptosites!,
+                      maptosites_d!,
+                      virial!,
+                      max_neigs,
+                      sites
+
 # TODO: implement site_energies, energy, forces, virial
+
+struct NBodyFunctionM
+   d::Dict{Tuple{Symbol,Symbol},NBodyFunction{2}}
+end
+
 
 
 # implement site_energies for given species
@@ -26,11 +38,38 @@ function site_energies(V::NBodyFunction{2}, at::Atoms{T},
    return Es
 end
 
+# using symbols
 function site_energies(V::NBodyFunction{2}, at::Atoms{T},
                                        species::Tuple{Symbol,Symbol}) where {T}
    sp = atomic_number.(species)
    return site_energies(V, at, sp)
 end
+
+energy(V::NBodyFunction, at::Atoms,species::Tuple{Symbol,Symbol}) = sum_kbn(site_energies(V, at,species))
+
+# Implementation of the forces
+function forces(V::NBodyFunction{2}, at::Atoms{T},sp::Tuple{Int,Int}) where { T}
+   nlist = neighbourlist(at, cutoff(V))
+   maxneigs = max_neigs(nlist)
+   F = zeros(JVec{T}, length(at))
+   dVsite = zeros(JVec{T}, maxneigs)
+   for (i, j, r, R) in sites(nlist)
+      fill!(dVsite, zero(JVec{T}))
+      eval_site_nbody!(
+            Val(2), R, cutoff(V),
+            (out, R, J, temp) -> evaluate_d!(out, V, R, J),
+            dVsite, nothing )   # dVsite == out, nothing == temp
+      # write site energy gradient into forces
+      for n = 1:length(j)
+         F[j[n]] -= dVsite[n]
+         F[i] += dVsite[n]
+      end
+   end
+   return F
+end
+
+
+
 
 r0 = 2.5
 V = bapolys(2, "($r0/r)^4", "(:cos, 3.6, 4.8)", 2)
@@ -44,29 +83,30 @@ Vcucu(3.)
 
 site_energies(Vcucu, at, (29,29))
 site_energies(Vcucu, at, (:Cu,:Cu))
+energy(Vcucu,at,(:Cu,:Cu))
+forces(Vcucu,at,(29,29))
 
-
-Vcucu(x) = x
-Vcuzr(x) = 2*x
-Vzrzr(x) = 3*x
-V2s = Dict(
-            (:Cu, :Cu) => Vcucu,
-            (:Cu, :Zr) => Vcuzr,
-            (:Zr, :Zr) => Vcuzr
-         )
-at = bulk(:Cu, cubic=true)*2
-rcut = 2.7
-
-Z = atomic_numbers(at)
-species = (:Cu,:Zn)
-for (i, j, r, R) in sites(atu, rcut)
-   Zi =
-   @show i
-   @show j
-   @show r
-   @show size(R)
-end
-
-sites(atu,rcut)
-site_energies(V2s, atu,rcut)
-energy(V2s,atu,rcut)
+# Vcucu(x) = x
+# Vcuzr(x) = 2*x
+# Vzrzr(x) = 3*x
+# V2s = Dict(
+#             (:Cu, :Cu) => Vcucu,
+#             (:Cu, :Zr) => Vcuzr,
+#             (:Zr, :Zr) => Vcuzr
+#          )
+# at = bulk(:Cu, cubic=true)*2
+# rcut = 2.7
+#
+# Z = atomic_numbers(at)
+# species = (:Cu,:Zn)
+# for (i, j, r, R) in sites(atu, rcut)
+#    Zi =
+#    @show i
+#    @show j
+#    @show r
+#    @show size(R)
+# end
+#
+# sites(atu,rcut)
+# site_energies(V2s, atu,rcut)
+# energy(V2s,atu,rcut)
