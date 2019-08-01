e3f237e2cce7faae12d4929ef4c0d7e49c1aad6e
Genevieve <g.dusson@warwick.ac.uk>
2019-08-01 10:48:32 +0200

    reg example + rename examples.

---
+++ examples/asm_env_reg.jl
@@ -0,0 +1,107 @@
+##
+@info("Load libraries ...")
+
+using JuLIP, NBodyIPs, NBIPsMulti, IPFitting
+
+@info("Load Butane database ...")
+
+include(homedir() * "/.julia/dev/NBIPsMulti/src/Butane.jl")
+data = Butane.load_xyz()
+data
+@show length(data)
+data[1]
+
+r0 = 3 .*round(rnn(:C),digits=2)
+
+D = Dict("E0" => Dict(:C => -1027.28368153, :H => -13.5203101677))
+oneB = MOneBody(D)
+
+rcut2 = 2.8 * r0
+rcut3 = 2.3 * r0
+rcut4 = 1.9 * r0
+
+@info("Generate descriptors...")
+
+BL2 = MultiDesc("exp( - 2 * (r/$r0-1))", "(:cos, $(rcut2-1.5), $(rcut2))",Val(:AA))
+
+BL3_AAA = MultiDesc("exp( - 2.5 * (r/$r0-1))", "(:cos2s, $(0.7*r0), $(0.88*r0), $(1.8*r0), $(rcut3))",Val(:AAA))
+
+BL3_AAB = MultiDesc("exp( - 2.5 * (r/$r0-1))", "(:cos2s, $(0.7*r0), $(0.88*r0), $(1.8*r0), $(rcut3))",Val(:AAB))
+
+BL4_AAAA = MultiDesc("exp( - 2.5 * (r/$r0-1))", "(:cos2s, $(0.7*r0), $(0.88*r0), $(1.8*r0), $(rcut3))",Val(:AAAA))
+
+BL4_AAAB = MultiDesc("exp( - 2.5 * (r/$r0-1))", "(:cos2s, $(0.7*r0), $(0.88*r0), $(1.8*r0), $(rcut3))",Val(:AAAB))
+
+BL4_AABB = MultiDesc("exp( - 2.5 * (r/$r0-1))", "(:cos2s, $(0.7*r0), $(0.88*r0), $(1.8*r0), $(rcut3))",Val(:AABB))
+
+Vn = ("1.0/(1.0 + exp(1.0 / ($(1.5*r0) - r + 1e-2)))", 1.5*r0)
+
+
+weights = Dict( (1,1)=> 1.0,
+                (1,6)=> 2.0,
+                (6,1)=> 2.0,
+                (6,6)=> 5.0
+                )
+
+##
+@info("Generate a 4B env basis ...")
+
+# basis = [
+#       envpolysM(BL2, 5, Vn, 2, weights, [6,6]);
+#       envpolysM(BL2, 5, Vn, 2, weights, [1,1]);
+#       envpolysM(BL2, 5, Vn, 2, weights, [1,6]);
+#       envpolysM(BL3_AAA, 3, Vn, 1, weights, [1,1,1]);
+#       envpolysM(BL3_AAA, 3,Vn, 1, weights, [6,6,6]);
+#       envpolysM(BL3_AAB, 3,Vn, 1, weights, [1,1,6]);
+#       envpolysM(BL3_AAB, 3,Vn, 1, weights, [1,6,6]);
+#       envpolysM(BL4_AAAA, 2,Vn, 1, weights, [1,1,1,1]);
+#       envpolysM(BL4_AAAA, 2,Vn, 1, weights, [6,6,6,6]);
+#       envpolysM(BL4_AAAB, 2,Vn, 1, weights, [1,1,1,6]);
+#       envpolysM(BL4_AAAB, 2,Vn, 1, weights, [1,6,6,6]);
+#       envpolysM(BL4_AABB, 2,Vn, 1, weights, [1,1,6,6]);
+#    ]
+
+basis = [
+      envpolysM(BL2, 10, Vn, 2, weights, [6,6]);
+      envpolysM(BL2, 10, Vn, 2, weights, [1,1]);
+      envpolysM(BL2, 10, Vn, 2, weights, [1,6]);
+      envpolysM(BL3_AAA, 5,Vn, 0, weights, [1,1,1]);
+      envpolysM(BL3_AAA, 5,Vn, 0, weights, [6,6,6]);
+      envpolysM(BL3_AAB, 5,Vn, 0, weights, [1,1,6]);
+      envpolysM(BL3_AAB, 5,Vn, 0, weights, [1,6,6]);
+      # envpolysM(BL4_AAAA, 4,Vn, 0, weights, [1,1,1,1]);
+      # envpolysM(BL4_AAAA, 4,Vn, 0, weights, [6,6,6,6]);
+      # envpolysM(BL4_AAAB, 4,Vn, 0, weights, [1,1,1,6]);
+      # envpolysM(BL4_AAAB, 4,Vn, 0, weights, [1,6,6,6]);
+      # envpolysM(BL4_AABB, 4,Vn, 0, weights, [1,1,6,6]);
+   ]
+
+
+@info("Assemble the LsqDB ...")
+@show length(basis)
+dbpath = homedir() * "/.julia/dev/NBIPsMulti/data/Butane_4B_env4"
+
+db =  LsqDB(dbpath, basis, data);
+db
+
+# -------------------------
+# To do the fit right away
+# -------------------------
+@info("Fit Butane Database basis...")
+
+obsweights = Dict("E" => 1.0, "F" => 1.0)
+configweights = Dict("nothing"  => 1.0)
+
+
+
+IP, lsqinfo = lsqfit( db; E0 = 0.,
+                       obsweights=obsweights,
+                       configweights=configweights,
+                       solver = (:rrqr, 1e-16),
+                       combineIP = NBodyIP,
+                       Vref = oneB,
+                       # Ibasis = collect(1:45)
+                       )
+
+errs = lsqinfo["errors"]
+rmse_table(rmse(errs)...)
---
+++ examples/comp_envpolys_nbpolys_env.jl
@@ -0,0 +1,176 @@
+@info("Load libraries...")
+
+using NBodyIPs
+using IPFitting
+using JuLIP
+using NBIPsMulti
+using StaticArrays
+using NBodyIPs.Regularisers: BLRegulariser, EnvBLRegulariser
+
+
+# -------------------------
+# For the 2 cases
+# -------------------------
+include(homedir() * "/.julia/dev/NBIPsMulti/src/Butane.jl")
+
+@info("Load database...")
+dbpath = homedir() * "/.julia/dev/NBIPsMulti/data/Butane_4B"
+
+data = Butane.load_xyz()
+
+r0 = 3*round(rnn(:C),digits=2)
+E0 = Butane.get_E0()
+rcut0 = 0.1
+
+rcut2 = 2.8 * r0
+rcut3 = 2.3 * r0
+# rcut4 = 1.9 * r0
+
+@info("Generate descriptors...")
+
+BL2 = MultiDesc(ExpTransform(2, r0), CosCut(rcut2-1.5,rcut2),Val(:AA))
+BL3_AAA = MultiDesc(ExpTransform(2.5,r0), CosCut(rcut3-1.5,rcut3),Val(:AAA))
+
+BL3_AAB = MultiDesc(ExpTransform(2.5,r0), CosCut(rcut3-1.5,rcut3),Val(:AAB))
+#
+# BL4_AAAA = MultiDesc(ExpTransform(2.5,r0), CosCut(rcut4-1.5,rcut4),Val(:AAAA))
+#
+# BL4_AAAB = MultiDesc(ExpTransform(2.5,r0), CosCut(rcut4-1.5,rcut4),Val(:AAAB))
+#
+# BL4_AABB = MultiDesc(ExpTransform(2.5,r0), CosCut(rcut4-1.5,rcut4),Val(:AABB))
+
+obsweights = Dict("E" => 1.0, "F" => 1.0)
+configweights = Dict("nothing"  => 1.0)
+
+oneB = MOneBody(Dict("E0" => Dict(:H => -13.5203101677, :C => -1027.28368153)))
+
+# -------------------------
+
+# -------------------------
+# For the 2 cases
+# -------------------------
+
+@info("Generate a 4B basis with nbpolys ...")
+basisnb = [
+      nbpolys(BL2, 14, [6,6]);
+      nbpolys(BL2, 14, [1,1]);
+      nbpolys(BL2, 14, [1,6]);
+      nbpolys(BL3_AAA, 3, [1,1,1]);
+      nbpolys(BL3_AAA, 3, [6,6,6]);
+      nbpolys(BL3_AAB, 3, [1,1,6]);
+      nbpolys(BL3_AAB, 3, [1,6,6]);
+      # nbpolys(BL4_AAAA, 2, [1,1,1,1]);
+      # nbpolys(BL4_AAAA, 2, [6,6,6,6]);
+      # nbpolys(BL4_AAAB, 2, [1,1,1,6]);
+      # nbpolys(BL4_AAAB, 2, [1,6,6,6]);
+      # nbpolys(BL4_AABB, 2, [1,1,6,6]);
+   ]
+
+regnbpol = [ BLRegulariser(2, rcut0, rcut3, npoints = 30, creg = 1.0,
+                transform = PolyTransform(1, r0), species = [1,6]),
+        BLRegulariser(2, rcut0, rcut3, npoints = 30, creg = 1.0,
+                        transform = PolyTransform(1, r0), species = [1,1]),
+        BLRegulariser(2, rcut0, rcut3, npoints = 30, creg = 1.0,
+                        transform = PolyTransform(1, r0), species = [6,6])                         ]
+
+
+@info("Assemble the LsqDB ...")
+@show length(basisnb)
+dbpathnb = homedir() * "/.julia/dev/NBIPsMulti/data/Butane_4B"
+
+dbnbpol =  LsqDB(dbpathnb, basisnb, data);
+dbnbpol
+
+# db = LsqDB(dbpath)
+# db
+# summary(db)
+##
+@info("Fit Butane Database basis...")
+
+
+
+IPnbpol, lsqinfonbpol = lsqfit( dbnbpol;
+                       # E0 = 1.,
+                       obsweights=obsweights,
+                       configweights=configweights,
+                       # solver = (:rrqr, 1e-16),
+                       solver = (:qr,),
+                       combineIP = NBodyIP,
+                       regularisers = regnbpol,
+                       # Ibasis = Ibasis
+                       Vref = oneB
+                       )
+
+errsnbpol = lsqinfonbpol["errors"]
+rmse_table(rmse(errsnbpol)...)
+
+# -------------------------
+# Environment-dependence
+# -------------------------
+
+
+Vn = ("1.0/(1.0 + exp(1.0 / ($(1.5*r0) - r + 1e-2)))", 1.5*r0)
+
+weights = Dict( (1,1)=> 1.0,
+                (1,6)=> 2.0,
+                (6,1)=> 2.0,
+                (6,6)=> 5.0
+                )
+
+basisenv = [
+    envpolysM(BL2, 14, Vn, 0, weights, [6,6]);
+    envpolysM(BL2, 14, Vn, 0, weights, [1,1]);
+    envpolysM(BL2, 14, Vn, 0, weights, [1,6]);
+    envpolysM(BL3_AAA, 3,Vn, 0, weights, [1,1,1]);
+    envpolysM(BL3_AAA, 3,Vn, 0, weights, [6,6,6]);
+    envpolysM(BL3_AAB, 3,Vn, 0, weights, [1,1,6]);
+    envpolysM(BL3_AAB, 3,Vn, 0, weights, [1,6,6]);
+    # envpolysM(BL4_AAAA, 4,Vn, 0, weights, [1,1,1,1]);
+    # envpolysM(BL4_AAAA, 4,Vn, 0, weights, [6,6,6,6]);
+    # envpolysM(BL4_AAAB, 4,Vn, 0, weights, [1,1,1,6]);
+    # envpolysM(BL4_AAAB, 4,Vn, 0, weights, [1,6,6,6]);
+    # envpolysM(BL4_AABB, 4,Vn, 0, weights, [1,1,6,6]);
+ ]
+
+ regenv = [ EnvBLRegulariser(2, 0, rcut0, rcut3, npoints = 30, creg = 1.0,
+                 transform = PolyTransform(1, r0), species = [1,6]),
+         EnvBLRegulariser(2, 0, rcut0, rcut3, npoints = 30, creg = 1.0,
+                         transform = PolyTransform(1, r0), species = [1,1]),
+         EnvBLRegulariser(2, 0, rcut0, rcut3, npoints = 30, creg = 1.0,
+                         transform = PolyTransform(1, r0), species = [6,6])                         ]
+
+@info("Assemble the environment LsqDB ...")
+@show length(basisenv)
+dbpathenv = homedir() * "/.julia/dev/NBIPsMulti/data/Butane_4B_env3"
+
+dbenv =  LsqDB(dbpathenv, basisenv, data);
+dbenv
+
+
+@info("Fit Butane Database basis with env dependence...")
+
+IPenv, lsqinfoenv = lsqfit( dbenv;
+                    # E0 = 1.,
+                    obsweights=obsweights,
+                    configweights=configweights,
+                    # solver = (:rrqr, 1e-16),
+                    solver = (:qr,),
+                    combineIP = NBodyIP,
+                    regularisers = regenv,
+                    # Ibasis = Ibasis
+                    Vref = oneB
+                    )
+
+errsenv = lsqinfoenv["errors"]
+rmse_table(rmse(errsenv)...)
+
+
+atX = SArray{Tuple{3},Float64,1,3}[[0.0, 0.0, 0.0], [0.6, 0.0, 0.0]]
+atZ = [6, 1]
+at = Atoms(atZ, atX; cell = [10 0 0;0 10 0;0 0 10])
+energy(IPnbpol, at)
+energy(IPenv, at)
+
+energy(IPnbpol, data[1].at)
+energy(IPenv, data[1].at)
+data[1].D["E"][1]
